7a3fdaeff6dfa2f26dd1977dbecfd3c528993602
Anthony Keenan <anthony.keenan@r3.com>
2018-05-02 18:20:56 +0100

    Make Kotlin Annotations Test more readable

--- api-scanner/src/test/java/net/corda/plugins/KotlinAnnotationsTest.java
+++ api-scanner/src/test/java/net/corda/plugins/KotlinAnnotationsTest.java
@@ -7,7 +7,10 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.rules.TestRule;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class KotlinAnnotationsTest {
@@ -17,37 +20,57 @@ public class KotlinAnnotationsTest {
     @Rule
     public TestRule rules = RuleChain.outerRule(testProjectDir).around(testProject);
 
+    private String[] expectedClassWithDeprecatedFunctions = new String[] {
+        "public final class net.corda.example.HasDeprecatedFunctions extends java.lang.Object",
+        "  public <init>()",
+        "  @NotNull",
+        "  public final String doSomething()",
+        "##"
+    };
+
+    private String[] expectedClassWithJvmField = new String[] {
+        "public final class net.corda.example.HasJvmField extends java.lang.Object",
+        "  public <init>()",
+        "  @NotNull",
+        "  public final String stringValue = \"Hello World\"",
+        "##"
+    };
+
+    private String[] expectedClassWithJvmStaticFunction = new String[]{
+        "public final class net.corda.example.HasJvmStaticFunction extends java.lang.Object",
+        "  public <init>()",
+        "  public static final void doThing(String)",
+        "  public static final net.corda.example.HasJvmStaticFunction$Companion Companion",
+        "##"
+    };
+
+    private String[] expectedClassWithJvmStaticFunctionCompanion = new String[] {
+        "public static final class net.corda.example.HasJvmStaticFunction$Companion extends java.lang.Object",
+        "  public final void doThing(String)",
+        "##"
+    };
+
+    private String[] expectedClassWithOverloadedConstructor = new String[] {
+        "public final class net.corda.example.HasOverloadedConstructor extends java.lang.Object",
+        "  public <init>()",
+        "  public <init>(String)",
+        "  public <init>(String, String)",
+        "  public <init>(String, String, int)",
+        "  @NotNull",
+        "  public final String getNotNullable()",
+        "  @Nullable",
+        "  public final String getNullable()",
+        "  public final int getNumber()",
+        "##"
+    };
+
     @Test
     public void testKotlinAnnotations() throws IOException {
-        assertEquals(
-            "public final class net.corda.example.HasDeprecatedFunctions extends java.lang.Object\n" +
-            "  public <init>()\n" +
-            "  @NotNull\n" +
-            "  public final String doSomething()\n" +
-            "##\n" +
-            "public final class net.corda.example.HasJvmField extends java.lang.Object\n" +
-            "  public <init>()\n" +
-            "  @NotNull\n" +
-            "  public final String stringValue = \"Hello World\"\n" +
-            "##\n" +
-            "public final class net.corda.example.HasJvmStaticFunction extends java.lang.Object\n" +
-            "  public <init>()\n" +
-            "  public static final void doThing(String)\n" +
-            "  public static final net.corda.example.HasJvmStaticFunction$Companion Companion\n" +
-            "##\n" +
-            "public static final class net.corda.example.HasJvmStaticFunction$Companion extends java.lang.Object\n" +
-            "  public final void doThing(String)\n" +
-            "##\n" +
-            "public final class net.corda.example.HasOverloadedConstructor extends java.lang.Object\n" +
-            "  public <init>()\n" +
-            "  public <init>(String)\n" +
-            "  public <init>(String, String)\n" +
-            "  public <init>(String, String, int)\n" +
-            "  @NotNull\n" +
-            "  public final String getNotNullable()\n" +
-            "  @Nullable\n" +
-            "  public final String getNullable()\n" +
-            "  public final int getNumber()\n" +
-            "##", CopyUtils.toString(testProject.getApi()));
+        assertThat(Files.readAllLines(testProject.getApi()))
+            .containsSequence(expectedClassWithDeprecatedFunctions)
+            .containsSequence(expectedClassWithJvmField)
+            .containsSequence(expectedClassWithJvmStaticFunction)
+            .containsSequence(expectedClassWithJvmStaticFunctionCompanion)
+            .containsSequence(expectedClassWithOverloadedConstructor);
     }
 }
