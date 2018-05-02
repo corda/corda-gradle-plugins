eb92032d4c6d78687500d528aba1c759c985775b
Anthony Keenan <anthony.keenan@r3.com>
2018-05-02 16:54:20 +0100

    remove unused namespacse

--- api-scanner/src/test/java/net/corda/plugins/KotlinVarargMethodTest.java
+++ api-scanner/src/test/java/net/corda/plugins/KotlinVarargMethodTest.java
@@ -1,21 +1,14 @@
 package net.corda.plugins;
 
-import org.gradle.testkit.runner.*;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TemporaryFolder;
 import org.junit.rules.TestRule;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 
-import static net.corda.plugins.CopyUtils.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class KotlinVarargMethodTest {
     private final TemporaryFolder testProjectDir = new TemporaryFolder();
