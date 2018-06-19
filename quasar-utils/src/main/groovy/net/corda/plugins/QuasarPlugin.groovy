c7ccfaee7a568912701272571d20d053b2aeff0b
Michele Sollecito <michele.sollecito@gmail.com>
2018-06-19 17:16:47 +0100

    [CORDA-1649]: Add default `quasar_version` and `quasar_group` to quasar-utils plugin, so it's not require to specify them manually. (#81)

--- quasar-utils/src/main/groovy/net/corda/plugins/QuasarPlugin.groovy
+++ quasar-utils/src/main/groovy/net/corda/plugins/QuasarPlugin.groovy
@@ -9,14 +9,21 @@ import org.gradle.api.tasks.JavaExec
  * QuasarPlugin creates a "quasar" configuration and adds quasar as a dependency.
  */
 class QuasarPlugin implements Plugin<Project> {
+
+    static defaultGroup = "co.paralleluniverse"
+    static defaultVersion = "0.7.10"
+
     @Override
     void apply(Project project) {
         Utils.createRuntimeConfiguration("cordaRuntime", project)
 
         project.configurations.create("quasar")
+
+        def quasarGroup = project.rootProject.hasProperty("quasar_group") ? project.rootProject.ext.quasar_group : defaultGroup
+        def quasarVersion = project.rootProject.hasProperty("quasar_version") ? project.rootProject.ext.quasar_version : defaultVersion
 //        To add a local .jar dependency:
 //        project.dependencies.add("quasar", project.files("${project.rootProject.projectDir}/lib/quasar.jar"))
-        project.dependencies.add("quasar", "${project.rootProject.ext.quasar_group}:quasar-core:${project.rootProject.ext.quasar_version}:jdk8@jar")
+        project.dependencies.add("quasar", "${quasarGroup}:quasar-core:${quasarVersion}:jdk8@jar")
         project.dependencies.add("cordaRuntime", project.configurations.getByName("quasar"))
 
         project.tasks.withType(Test) {
