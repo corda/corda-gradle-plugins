a093297154a94414ea74ac4e299f06960639cad6
Clinton Alexander <clinton.alexander@r3.com>
2018-08-22 18:09:43 +0100

    Removed cordform common and cordform definition

--- cordformation/src/main/kotlin/net/corda/plugins/Node.kt
+++ cordformation/src/main/kotlin/net/corda/plugins/Node.kt
@@ -2,8 +2,6 @@ package net.corda.plugins
 
 import com.typesafe.config.*
 import groovy.lang.Closure
-import net.corda.cordform.CordformNode
-import net.corda.cordform.RpcSettings
 import net.corda.plugins.utils.copyTo
 import net.corda.plugins.utils.plus
 import org.apache.commons.io.FilenameUtils.removeExtension
