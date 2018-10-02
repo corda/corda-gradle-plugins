896d1a3c4fd76208195d9e9ace6158e40db9d950
Florian Friemel <florian.friemel@r3.com>
2018-10-02 17:13:06 +0100

    Do not attempt to set the target version if not specified.

--- cordapp/src/main/kotlin/net/corda/plugins/CordappPlugin.kt
+++ cordapp/src/main/kotlin/net/corda/plugins/CordappPlugin.kt
@@ -7,10 +7,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.jvm.tasks.Jar
 import java.io.File
-import java.util.jar.JarInputStream
 
 /**
  * The Cordapp plugin will turn a project into a cordapp project which builds cordapp JARs with the correct format
@@ -52,7 +50,7 @@ class CordappPlugin : Plugin<Project> {
         val task = project.task("configureCordappFatJar")
         val jarTask = project.tasks.getByName("jar") as Jar
         jarTask.doFirst {
-            val (targetVersion, minPlatformVersion) = getAndCheckVersionInfo(project)
+            val (targetVersion, minPlatformVersion) = checkVersionInfo()
             val attributes = jarTask.manifest.attributes
             attributes["Name"] = cordapp.info?.name ?: "${project.group}.${jarTask.baseName}"
             attributes["Implementation-Version"] = cordapp.info?.version ?: project.version
@@ -123,28 +121,10 @@ class CordappPlugin : Plugin<Project> {
         return filteredDeps.toUniqueFiles(runtimeConfiguration) - excludeDeps.toUniqueFiles(runtimeConfiguration)
     }
 
-    private fun getAndCheckVersionInfo(project: Project): Pair<Int?, Int?> {
-        // If the target version is not set, try to determine the platformversion of the Corda jar this project
-        // depends on and use that.
-        val targetVersion: Int? =
-                if (cordapp.info?.targetVersion == null) {
-                    project.logger.warn("No target version specified. Attempting to determine platform version from the project's Corda dependency.")
-                    val platformVersion = getPlatformVersion(project)
-                    if (platformVersion == null) {
-                        project.logger.warn("Could not determine platform version from the project's Corda dependency.")
-                    } else {
-                        project.logger.info("Setting target version to ${platformVersion}.")
-                    }
-                    platformVersion
-                } else {
-                    cordapp.info?.targetVersion
-                }
-
+    private fun checkVersionInfo(): Pair<Int?, Int?> {
         // If the minimum platform version is not set, default to 1.
         val minPlatformVersion: Int = cordapp.info?.minPlatformVersion ?: 1
-        if (targetVersion == null) {
-            throw InvalidUserDataException("Target version was not set and could not be determined from the project's Corda dependency. Please specify the target version of your CorDapp.")
-        }
+        val targetVersion = cordapp.info?.targetVersion ?: throw InvalidUserDataException("Target version was not set and could not be determined from the project's Corda dependency. Please specify the target version of your CorDapp.")
         if (targetVersion < 1) {
             throw InvalidUserDataException("Target version must not be smaller than 1.")
         }
@@ -152,31 +132,6 @@ class CordappPlugin : Plugin<Project> {
             throw InvalidUserDataException("Target version must not be smaller than min platform version.")
         }
         return Pair(targetVersion, minPlatformVersion)
-    }
-
-    /**
-     * Retrieve the Corda platform version from the project's Corda jar (if present)
-     *
-     * @param project The project environment this plugin executes in.
-     * @return The platform version of the projects Corda jar, or null if it cannot be determined.
-     */
-    private fun getPlatformVersion(project: Project): Int? {
-        val releaseVersion =
-                try {
-                    project.rootProject.ext<String>("corda_release_version")
-                } catch (e: ExtraPropertiesExtension.UnknownPropertyException) {
-                    project.logger.warn(e.message)
-                    null
-                } ?: return null
-        val maybeJar = project.configuration("runtime").filter {
-            "corda-$releaseVersion.jar" in it.toString() || "corda-enterprise-$releaseVersion.jar" in it.toString()
-        }
-        if (!maybeJar.isEmpty && maybeJar.singleFile.isFile) {
-            return maybeJar.singleFile.toURI().toURL()
-                    .openStream().let(::JarInputStream)
-                    .use { it.manifest }.mainAttributes?.getValue("Corda-Platform-Version")?.toInt()
-        }
-        return null
     }
 
     private fun Iterable<Dependency>.toUniqueFiles(configuration: Configuration): Set<File> {
