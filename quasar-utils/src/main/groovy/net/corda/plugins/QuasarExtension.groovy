cda548dc7be7b9803785ba25d5ae4ed5bb0df9f7
Chris Rankin <chris.rankin@r3.com>
2019-05-20 15:16:18 +0100

    CORDA-2948: Add Quasar options for debug and verbose. (#221) (#222)

--- quasar-utils/src/main/groovy/net/corda/plugins/QuasarExtension.groovy
+++ quasar-utils/src/main/groovy/net/corda/plugins/QuasarExtension.groovy
@@ -2,22 +2,41 @@ package net.corda.plugins
 
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 
 import javax.inject.Inject
 
 class QuasarExtension {
 
+    final Property<Boolean> debug
+
+    final Property<Boolean> verbose
+
     final ListProperty<String> excludePackages
 
-    final Provider<String> exclusions
+    final Provider<String> options
 
     @Inject
     QuasarExtension(ObjectFactory objects, Iterable<? extends String> initialExclusions) {
+        debug = objects.property(Boolean)
+        debug.set(false)
+        verbose = objects.property(Boolean)
+        verbose.set(false)
         excludePackages = objects.listProperty(String)
         excludePackages.set(initialExclusions)
-        exclusions = excludePackages.map { excludes ->
-            excludes.isEmpty() ? '' : "=x(${excludes.join(';')})".toString()
+        options = excludePackages.map { excludes ->
+            def builder = new StringBuilder('=')
+            if (debug.get()) {
+                builder.append('d')
+            }
+            if (verbose.get()) {
+                builder.append('v')
+            }
+            if (!excludes.isEmpty()) {
+                builder.append('x(').append(excludes.join(';')).append(')')
+            }
+            builder.length() == 1 ? '' : builder.toString()
         }
     }
 }
