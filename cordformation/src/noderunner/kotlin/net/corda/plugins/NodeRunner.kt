2d2bd4d4c221f123fc5db40058fe7391541d7f92
Tudor Malene <tudor.malene@r3.com>
2019-09-16 16:05:51 +0100

    CORDA-3024 Rename corda-webserver
    CORDA-3024 Rename corda-webserver
    
    CORDA-3024 Address comment
    
    CORDA-3024 Address comment

--- cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
+++ cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
@@ -7,7 +7,7 @@ import java.util.*
 private const val HEADLESS_FLAG = "--headless"
 private const val CAPSULE_DEBUG_FLAG = "--capsule-debug"
 private const val CORDA_JAR_NAME = "corda.jar"
-private const val CORDA_WEBSERVER_JAR_NAME = "corda-webserver.jar"
+private const val CORDA_WEBSERVER_JAR_NAME = "corda-testserver.jar"
 private const val CORDA_CONFIG_NAME = "node.conf"
 private const val CORDA_WEBSERVER_CONFIG_NAME = "web-server.conf"
 private val CORDA_HEADLESS_ARGS = listOf("--no-local-shell")
