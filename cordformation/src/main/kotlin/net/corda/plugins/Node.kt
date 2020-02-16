6924c5b6d2b9196fe9bcd9ddc91c0ce80c8b811e
Eduard Klementiev <e.klementiev@firmshift.com>
2020-02-16 21:07:59 +0300

    Use config fallback for defaults

--- cordformation/src/main/kotlin/net/corda/plugins/Node.kt
+++ cordformation/src/main/kotlin/net/corda/plugins/Node.kt
@@ -562,15 +562,16 @@ open class Node @Inject constructor(private val project: Project) {
         if (!config.hasPath("sshd.port")) {
             sshdPort(defaultSsh)
         }
+        val configDefaults = ConfigFactory.empty()
+              .withValue("dataSourceProperties.dataSource.url", ConfigValueFactory.fromAnyRef("jdbc:h2:file:./persistence/persistence;DB_CLOSE_ON_EXIT=FALSE;WRITE_DELAY=0;LOCK_TIMEOUT=10000"))
         val dockerConf = config
                 .withValue("p2pAddress", ConfigValueFactory.fromAnyRef("$containerName:$p2pPort"))
                 .withValue("rpcSettings.address", ConfigValueFactory.fromAnyRef("$containerName:${rpcSettings.port}"))
                 .withValue("rpcSettings.adminAddress", ConfigValueFactory.fromAnyRef("$containerName:${rpcSettings.adminPort}"))
                 .withValue("detectPublicIp", ConfigValueFactory.fromAnyRef(false))
+                .withFallback(configDefaults)
+
         config = dockerConf
-        if (!config.hasPath("dataSourceProperties.dataSource.url")) {
-            config = config.withValue("dataSourceProperties.dataSource.url", ConfigValueFactory.fromAnyRef("jdbc:h2:file:./persistence/persistence;DB_CLOSE_ON_EXIT=FALSE;WRITE_DELAY=0;LOCK_TIMEOUT=10000"))
-        }
         createNodeAndWebServerConfigFiles(config)
     }
 
