26c953c9d866722c64256f0927af1c327bc32161
Chris Rankin <chris.rankin@r3.com>
2020-07-09 15:40:00 +0100

    CORDA-3902: Update publish-utils to be a semi-respectable Gradle citizen. (#335)

--- publish-utils/src/main/groovy/net/corda/plugins/publish/bintray/License.groovy
+++ publish-utils/src/main/groovy/net/corda/plugins/publish/bintray/License.groovy
@@ -1,16 +1,41 @@
 package net.corda.plugins.publish.bintray
 
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+
+import javax.inject.Inject
+
+@SuppressWarnings("unused")
 class License {
+    private final Property<String> name
+    private final Property<String> url
+    private final Property<String> distribution
+
+    @Inject
+    License(ObjectFactory objects) {
+        name = objects.property(String.class)
+        url = objects.property(String.class)
+        distribution = objects.property(String.class)
+    }
+
     /**
      * The name of license (eg; Apache 2.0)
      */
-    String name
+    Property<String> getName() {
+        return name
+    }
+
     /**
      * URL to the full license file
      */
-    String url
+    Property<String> getUrl() {
+        return url
+    }
+
     /**
      * The distribution level this license corresponds to (eg: repo)
      */
-    String distribution
+    Property<String> getDistribution() {
+        return distribution
+    }
 }
