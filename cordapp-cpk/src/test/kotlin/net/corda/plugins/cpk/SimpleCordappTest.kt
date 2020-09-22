be86756d51b603feb43a12aac1ba252ea6442adf
Chris Rankin <chris.rankin@r3.com>
2020-09-22 19:17:47 +0100

    Use Bnd to generate OSGi metadata for the CPK CorDapp jar.

--- cordapp-cpk/src/test/kotlin/net/corda/plugins/cpk/SimpleCordappTest.kt
+++ cordapp-cpk/src/test/kotlin/net/corda/plugins/cpk/SimpleCordappTest.kt
@@ -1,11 +1,14 @@
 package net.corda.plugins.cpk
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestReporter
 import org.junit.jupiter.api.io.TempDir
+import org.osgi.framework.Constants.*
 import java.nio.file.Path
+import java.util.jar.JarFile
 
 class SimpleCordappTest {
     companion object {
@@ -17,6 +20,7 @@ class SimpleCordappTest {
         fun setup(@TempDir testProjectDir: Path, reporter: TestReporter) {
             testProject = GradleProject(testProjectDir, reporter)
                 .withTestName("simple-cordapp")
+                .withSubResource("src/main/java/com/example/contract/ExampleContract.java")
                 .build(
                     "-Pcordapp_contract_version=$expectedCordappContractVersion",
                     "-Pcorda_release_version=$cordaReleaseVersion"
@@ -33,10 +37,23 @@ class SimpleCordappTest {
         val artifacts = testProject.artifacts
         assertThat(artifacts).hasSize(2)
 
+        val cpk = artifacts.single { it.toString().endsWith(".cpk") }
+        assertThat(cpk).isRegularFile()
+
         val cordapp = artifacts.single { it.toString().endsWith(".jar") }
         assertThat(cordapp).isRegularFile()
 
-        val cpk = artifacts.single { it.toString().endsWith(".cpk") }
-        assertThat(cpk).isRegularFile()
+        val jarManifest = JarFile(cordapp.toFile()).use(JarFile::getManifest)
+        println(jarManifest.mainAttributes.entries)
+
+        with(jarManifest.mainAttributes) {
+            assertEquals("Simple", getValue(BUNDLE_NAME))
+            assertEquals("com.example.simple-cordapp", getValue(BUNDLE_SYMBOLICNAME))
+            assertEquals("1.0.1.SNAPSHOT", getValue(BUNDLE_VERSION))
+            assertEquals("net.corda.core.contracts,net.corda.core.transactions,org.apache.commons.io;version=\"[1.4,2)\"", getValue(IMPORT_PACKAGE))
+            assertEquals("com.example.contract", getValue("Private-Package"))
+            assertEquals("osgi.ee;filter:=\"(&(osgi.ee=JavaSE)(version=1.8))\"", getValue(REQUIRE_CAPABILITY))
+            assertEquals("true", getValue("Sealed"))
+        }
     }
 }
