fd71e4ba9c6035e87b43c8a35cd3749e1df2981a
Chris Rankin <chris.rankin@r3.com>
2020-10-26 21:36:33 +0000

    Check that Gradle is compatible with the CPK plugin.

--- cordapp-cpk/src/main/kotlin/net/corda/plugins/cpk/CordappUtils.kt
+++ cordapp-cpk/src/main/kotlin/net/corda/plugins/cpk/CordappUtils.kt
@@ -5,7 +5,6 @@ import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.plugins.JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME
 import org.gradle.api.plugins.JavaPlugin.RUNTIME_ONLY_CONFIGURATION_NAME
-import kotlin.math.max
 
 const val GROUP_NAME = "Cordapp"
 
@@ -13,26 +12,6 @@ const val CORDAPP_CONFIGURATION_NAME = "cordapp"
 const val CORDA_RUNTIME_ONLY_CONFIGURATION_NAME = "cordaRuntimeOnly"
 const val CORDA_PROVIDED_CONFIGURATION_NAME = "cordaProvided"
 const val CORDA_CPK_CONFIGURATION_NAME = "cordaCPK"
-
-fun compareVersions(v1: String, v2: String): Int {
-    val parsed1 = parseVersionString(v1)
-    val parsed2 = parseVersionString(v2)
-    for (i in 0 until max(parsed1.count(), parsed2.count())) {
-        val compareResult = parsed1.getOrElse(i) { 0 }.compareTo(parsed2.getOrElse(i) { 0 })
-        if (compareResult != 0) {
-            return compareResult
-        }
-    }
-    return 0
-}
-
-private fun parseVersionString(v: String) = v.split('.').flatMap { it.split('-') }.map {
-    try {
-        it.toInt()
-    } catch (e: NumberFormatException) {
-        -1
-    }
-}
 
 private fun ConfigurationContainer.createChildConfiguration(name: String, parent: Configuration): Configuration {
     return findByName(name) ?: run {
