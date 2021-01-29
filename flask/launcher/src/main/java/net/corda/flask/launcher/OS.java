b9c89cf403e87e33a48ce6c02db6c6976b87cd61
Walter Oggioni <walter.oggioni@r3.com>
2021-01-29 21:15:29 +0100

    fixed OS recognition on OSX

--- flask/launcher/src/main/java/net/corda/flask/launcher/OS.java
+++ flask/launcher/src/main/java/net/corda/flask/launcher/OS.java
@@ -1,26 +1,13 @@
 package net.corda.flask.launcher;
 
-import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum OS {
-    WINDOWS("windows"),
-    MACOS("macos"),
-    LINUX("linux"),
-    SOLARIS("solaris"),
-    BSD("bsd"),
-    AIX("aix"),
-    HP_UX("hp-ux"),
-    UNIX("unix"),
-    POSIX("posix"),
-    VMS("vms");
-
-    private final String value;
-
-    OS(String value) {
-        this.value = value;
-    }
+    WINDOWS, MACOS, LINUX, SOLARIS, BSD, AIX, HP_UX, VMS, UNKNOWN;
 
     public static OS current;
     public static boolean isUnix;
@@ -28,13 +15,28 @@ public enum OS {
     public static boolean isMac;
 
     static {
+        Logger log = LoggerFactory.getLogger(OS.class);
         String osName = System.getProperty("os.name").toLowerCase();
-        for(OS os : values()) {
-            if(Objects.equals(osName, os.value)) {
-                current = os;
-            }
+
+        if (osName.startsWith("windows"))
+            current = WINDOWS;
+        if (osName.startsWith("mac"))
+            current = MACOS;
+        if (osName.contains("linux"))
+            current = LINUX;
+        if (osName.contains("solaris") || osName.contains("sunos") || osName.contains("illumos"))
+            current = SOLARIS;
+        if (osName.contains("bsd"))
+            current = BSD;
+        if (osName.contains("aix"))
+            current = AIX;
+        if (osName.contains("hp-ux"))
+            current = HP_UX;
+        if (osName.contains("vms"))
+            current = VMS;
+        if(current == null)  {
+            log.error("Unrecognized OS '{}'", osName);
         }
-        if(current == null) throw new IllegalArgumentException(String.format("Unrecognized OS '%s'", osName));
         isUnix = Stream.of(LINUX, SOLARIS, BSD, AIX, HP_UX).collect(Collectors.toSet()).contains(current);
         isWindows = current == WINDOWS;
         isMac = current == MACOS;
