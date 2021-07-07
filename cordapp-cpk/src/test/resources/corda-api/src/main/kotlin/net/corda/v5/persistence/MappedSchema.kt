91af1523a1260bc4ad112313ac73a9cceca202c1
Chris Rankin <chris.rankin@r3.com>
2021-07-07 12:25:40 +0100

    CORE-268: Update cordapp-cpk for new location of MappedSchema class.

--- cordapp-cpk/src/test/resources/corda-api/src/main/kotlin/net/corda/v5/persistence/MappedSchema.kt
+++ cordapp-cpk/src/test/resources/corda-api/src/main/kotlin/net/corda/v5/persistence/MappedSchema.kt
@@ -1,5 +1,5 @@
 @file:Suppress("PackageDirectoryMismatch")
-package net.corda.v5.application.services.persistence
+package net.corda.v5.persistence
 
 open class MappedSchema(schemaFamily: Class<*>, val version: Int, val mappedTypes: Iterable<Class<*>>) {
     val name: String = schemaFamily.name
