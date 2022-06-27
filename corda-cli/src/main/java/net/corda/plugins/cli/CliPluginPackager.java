1e3ec97f7f939584de6a312f243553c9017db908
Peter McKinney <pmck91@users.noreply.github.com>
2022-06-27 10:46:25 +0100

    Java version to replace groovy (WIP) not finished.

--- corda-cli/src/main/java/net/corda/plugins/cli/CliPluginPackager.java
+++ corda-cli/src/main/java/net/corda/plugins/cli/CliPluginPackager.java
@@ -15,11 +15,18 @@ public class CliPluginPackager implements Plugin<Project> {
     public void apply(Project project) {
         project.getPluginManager().apply(JavaPlugin.class);
 
+        CliPluginPackagerExtension cliPluginPackagerExtension = project.getExtensions().create("cliPlugin", CliPluginPackagerExtension.class);
+
         project.getTasks().named("CliPluginPackage", Jar.class, jar -> {
             jar.getArchiveBaseName().set("plugin");
 
             jar.manifest(manifest -> {
-                Attributes attributes = manifest.getAttributes()
+                Attributes attributes = manifest.getAttributes();
+                attributes.put("Plugin-Class", cliPluginPackagerExtension.getCliPluginClass());
+                attributes.put("Plugin-Id", cliPluginPackagerExtension.getCliPluginId());
+                attributes.put("Plugin-Version", project.getVersion());
+                attributes.put("Plugin-Provider", cliPluginPackagerExtension.getCliPluginProvider());
+                attributes.put("Plugin-Description", cliPluginPackagerExtension.getCliPluginDescription());
             });
 
             SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
@@ -32,7 +39,12 @@ public class CliPluginPackager implements Plugin<Project> {
 
             jar.dependsOn(runtimeConfig);
 
-            jar.from(runtimeConfig);
+            jar.from(runtimeConfig.fileCollection())
+                    .exclude("META-INF/*.SF")
+                    .exclude("META-INF/*.DSA")
+                    .exclude("META-INF/*.RSA")
+                    .exclude("module-info.class")
+                    .exclude("META-INF/versions/*/module-info.class");
         });
     }
 }
