2292f74d9b6f3be9b5ac0986c67ddff93f851ba1
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-07-10 17:52:03 +0100

    CORE-4355: CPK Format version 2 (#512)
    * CPK Format version 2
    * Add Corda-CPB-Format = 2.0 to the CPB manifest
    * Use constant as expected value in unit test

--- cordapp-cpk2/src/test/kotlin/net/corda/plugins/cpb2/CpbTest.kt
+++ cordapp-cpk2/src/test/kotlin/net/corda/plugins/cpb2/CpbTest.kt
@@ -1,5 +1,8 @@
 package net.corda.plugins.cpb2
 
+import net.corda.plugins.cpb2.CpbTask.Companion.CPB_CURRENT_FORMAT_VERSION
+import net.corda.plugins.cpb2.CpbTask.Companion.CPB_FORMAT_VERSION
+import net.corda.plugins.cpb2.CpbTask.Companion.CPB_VERSION_ATTRIBUTE
 import net.corda.plugins.cpk2.GradleProject
 import net.corda.plugins.cpk2.cordaApiVersion
 import net.corda.plugins.cpk2.digestFor
@@ -100,7 +103,8 @@ class CpbTest {
         val embeddedCpkFiles = TreeMap<String, String>()
         JarInputStream(Files.newInputStream(cpbFile), true).use { jarInputStream ->
             assertEquals("customName", jarInputStream.manifest.mainAttributes.getValue("Corda-CPB-Name"))
-            assertEquals("customVersion", jarInputStream.manifest.mainAttributes.getValue("Corda-CPB-Version"))
+            assertEquals("customVersion", jarInputStream.manifest.mainAttributes.getValue(CPB_VERSION_ATTRIBUTE))
+            assertEquals(CPB_CURRENT_FORMAT_VERSION, jarInputStream.manifest.mainAttributes.getValue(CPB_FORMAT_VERSION))
             generateSequence(jarInputStream::getNextJarEntry).forEach { jarEntry ->
                 when {
                     jarEntry.name.endsWith(".cpk") -> {
