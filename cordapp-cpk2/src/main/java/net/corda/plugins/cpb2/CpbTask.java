ea0a90d4fb6269d87c341fa164a89ae3cd7d4705
malachy <malachy.byrne@r3.com>
2023-11-01 14:22:26 +0000

    Remove unused import

--- cordapp-cpk2/src/main/java/net/corda/plugins/cpb2/CpbTask.java
+++ cordapp-cpk2/src/main/java/net/corda/plugins/cpb2/CpbTask.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.jar.JarInputStream;
 
