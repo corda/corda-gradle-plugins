be5810d7e4e05b77030481ee1c9b6d05bd812411
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2024-03-18 13:53:36 +0000

    NOTICK: Delete excluded projects

--- cordformation/src/test/scripts/generate-keystore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-KEYPASS=MyStorePassword
-STOREPASS=MyStorePassword
-ALIAS=MyKeyAlias
-
-rm -f testkeystore
-
-keytool -keystore testkeystore -storetype pkcs12 -genkey -dname 'CN=localhost, O=R3, L=London, C=UK' -keyalg RSA -alias ${ALIAS} -validity 3650 -keypass ${KEYPASS} -storepass ${STOREPASS}
